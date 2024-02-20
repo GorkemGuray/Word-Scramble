b67ec2f5819b7a6c297663c83e607168aef1ada1
Görkem Güray <grkmgry@gmail.com>
2024-02-20 22:45:42 +0300

    Initial Commit

---
+++ Word Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word Scramble
+//
+//  Created by Görkem Güray on 20.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
